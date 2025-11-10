81ed0f586a2f953f61b11e9d2497305455869f55
sowTecsup <jneyra@tecsup.edu.pe>
2025-11-10 09:19:44 -0500

    Binary Tree

--- Assets/Scripts/GameManger.cs
+++ Assets/Scripts/GameManger.cs
@@ -3,25 +3,40 @@ using UnityEngine;
 public class GameManger : MonoBehaviour
 {
     private Tree<string> namesTree;
+
+    BinaryTree<int> rootBinaryTree = new(8);
     void Start()
     {
-        string tarversePre = "";
+        /*string tarversePre = "";
         namesTree.TraversePreOrder(namesTree.Root, v => 
         {
          tarversePre += v + ", ";
         });
-        print(tarversePre);
+        print(tarversePre);*/
+
+
+
+
+
+        rootBinaryTree.AddChild(8, 3, true);
+        rootBinaryTree.AddChild(8, 10, false);
+
+        rootBinaryTree.AddChild(3, 1, true);
+        rootBinaryTree.AddChild(3, 6, false);
+
+        rootBinaryTree.AddChild(6, 4, true);
+        rootBinaryTree.AddChild(6, 7, false);
+
+
+        rootBinaryTree.AddChild(10, 14);
+
+        rootBinaryTree.AddChild(14, 13);
+
+        rootBinaryTree.TraverseInOrder(rootBinaryTree.Root, v => Debug.Log(v));
+
+
     }
 
    
 }
 
-public class PlayerData
-{
-    public int SkillLevel;
-
-    public void ClearData()
-    {
-
-    }
-}
